e04ca0a78ee0126a6227873b1d5b5ece287b6fd4
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-03 15:28:16 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/shared/Navbar";
 import Header from "@/components/Header";
- 
+
 export default function Home() {
   return (
-    <div className="bg-light-yellow h-screen">
-      <Navbar/> 
-      <Header/>
+    <div className="bg-yellow-200 min-h-screen">
+      <Navbar /> 
+      <Header />
+      {/* Add more content here */}
     </div>
   );
 }
