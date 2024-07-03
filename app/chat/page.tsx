256aa349e71e0a8c93b1d701504162cc507ce1d9
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-03 15:33:25 +0530

    last update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,13 +1,24 @@
-import ChatCamera from "@/components/ChatCamera"
+import ChatCamera from "@/components/ChatCamera";
+import Background from "@/public/background.jpg";
+import Image from 'next/image';
 
 const ChatPage = () => {
     return (
         <main className="flex flex-grow items-center">
-            <div className="bg-chatImage bg-cover bg-right-bottom rounded-lg w-full h-[98%] flex mx-2 items-center justify-center px-6">
-                <ChatCamera />
+            {/* Background Image */}
+            <div className="relative w-full h-[98%] mx-2">
+                {/* Absolute positioning for background image */}
+                <div className="absolute inset-0 bg-cover bg-right-bottom rounded-lg">
+                    <Image src={Background} layout="fill" objectFit="cover" alt="background" />
+                </div>
+                
+                {/* Chat Camera Component */}
+                <div className="relative w-full h-full flex items-center justify-center px-6">
+                    <ChatCamera />
+                </div>
             </div>
         </main>
     );
 }
 
-export default ChatPage
+export default ChatPage;
