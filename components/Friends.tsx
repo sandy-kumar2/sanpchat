f052ca724741448a7572e3de83fc5c46ab41f08e
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-10 01:59:38 +0530

    Update

--- components/Friends.tsx
+++ components/Friends.tsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import Friend from './Friend'
-import { auth } from '@/auth'
+import React, { useEffect, useState } from 'react';
+import Friend from './Friend';
+import { auth } from '@/auth';
 import { getSidebarUsers } from '@/lib/userdata';
- 
-// const sleep = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const Friends = async () => { 
-  const authUser = await auth();
-  // sleep(1000);
-  const otherUsers:any =  authUser?.user ? await getSidebarUsers(authUser?.user?._id) : [];
+const Friends = () => {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const authUser = await auth();
+        if (authUser?.user?._id) {
+          const otherUsers = await getSidebarUsers(authUser.user._id);
+          setUsers(otherUsers);
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []); // Run once on component mount
 
   return (
-    <div className='flex-1 overflow-y-auto '>
-      {
-        otherUsers?.map((user: any) => {
-          return (
-            <Friend key={user._id} user={user}/>
-          )
-        })
-      }
+    <div className='flex-1 overflow-y-auto'>
+      {users.map((user) => (
+        <Friend key={user._id} user={user} />
+      ))}
     </div>
-  )
-}
-export default Friends
+  );
+};
+
+export default Friends;
