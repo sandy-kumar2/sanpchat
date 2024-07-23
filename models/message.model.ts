69840a13df09d816ac1d30f2560c306fad8a1f98
sandy-kumar2 <imsandeep721@gmail.com>
2024-07-23 14:50:16 +0530

    update

--- models/message.model.ts
+++ models/message.model.ts
@@ -1,44 +1,39 @@
-import mongoose, { Types, Document, Model, PopulatedDoc } from "mongoose";
-import { UserDocument } from "./user.model";
+import mongoose, { Types, Document, Model, PopulatedDoc } from "mongoose"
+import { UserDocument } from "./user.model"
 
-export interface MessageInterface {
-    senderId: Types.ObjectId | PopulatedDoc<UserDocument>;
-    receiverId: Types.ObjectId | PopulatedDoc<UserDocument>;
-    content: string;
-    messageType: 'text' | 'image';
-    opened: boolean;
+export interface MessageInterface{
+    senderId:Types.ObjectId | PopulatedDoc<UserDocument>,
+    receiverId:Types.ObjectId | PopulatedDoc<UserDocument>,
+    content:string,
+    messageType: 'text' | 'image',
+    opened:boolean
 }
-
-export interface MessageDocument extends MessageInterface, Document {
+export interface MessageDocument extends MessageInterface, Document{
     _id: Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt:Date,
+    updatedAt:Date
 }
-
-const messageSchema = new mongoose.Schema<MessageDocument>({
-    senderId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+const messageModel = new mongoose.Schema<MessageDocument>({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
     },
-    receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+    receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
     },
-    content: {
-        type: String,
-        required: true,
+    content:{
+        type:String,
+        required:true,
     },
-    messageType: {
-        type: String,
-        required: true,
-        enum: ['text', 'image'],
+    messageType:{
+        type:String,
+        required:true,
+        enum:['text', 'image']
     },
-    opened: {
-        type: Boolean,
-        default: false,
-    },
-}, { timestamps: true });
-
-export const Message: Model<MessageDocument> = mongoose.models.Message || mongoose.model('Message', messageSchema);
+    opened:{
+        type:Boolean,
+        default:false
+    }
+}, {timestamps:true});
+export const Message : Model<MessageDocument> = mongoose.models?.Message || mongoose.model('Message', messageModel);
